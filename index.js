b8d8d511544e8b36b9f01c6e869e64fdfcbbe852
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-03-30 01:24:13 -0500

    Implementar control de puerto, default 3000

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const argv = require('./config/yargs').argv;
 
-app.get('/', function(req, res) {
 
-    res.send('Hello World');
+console.log("Command: ", argv._[0]);
+
+let APP_PORT = 3000;
+
+if( argv.port ) {
+    APP_PORT = argv.port;
+}
+
+app.get('/function-test', function(req, res) {
+
+    console.log("Entrando a la funcion...", new Date().toUTCString);
+
+    res.send('Hello World, my name is George!');
 
 });
 
-app.listen(3000);
+console.log( "Aplicación corriendo en el puerto ", APP_PORT );
+
+app.listen( APP_PORT );
